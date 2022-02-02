683913d06853e0409d1618d45d61f2f428fc666b
mohamed muflahi <muflahimohamed88@gmail.com>
2022-02-02 11:48:41 -0500

    commit tree 7352c6f51244ab8b71ff415141a80f344d60fc02

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+let superbowlWin = (arr) =>{
+    let win = arr.find(finder);
+    if (win != undefined){
+        return win["year"];
+    }else{
+        return undefined;
+    }
+}
+function finder(arr){
+    return arr.result === "W";
+}
+
